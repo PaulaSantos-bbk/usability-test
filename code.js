ec33aa3c670a56b1a88749324515e448d2be478a
unknown <pangeriz@outlook.com>
2020-02-21 19:16:27 +0000

    responsive menus

--- code.js
+++ code.js
@@ -215,3 +215,12 @@ function FormJS() {
     alert("Thank you for getting in touch with us " + jFirst);
 }
 
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    let x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
